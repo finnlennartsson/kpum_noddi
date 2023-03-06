420b7f455b291b9c059f4fdbc24c652b9f90772f
Finn Lennartsson <finn.lennartsson@gmail.com>
2023-03-06 19:39:33 +0900

    noddi.m matlab routine

---
+++ matlab/noddi.m
@@ -0,0 +1,12 @@
+% cd to session dir and initialize 3D grey view 
+SubjPATH =  '/home/finn/Research/Projects/Kyoto_Collab/KPUM_NODDI/data/derivatives/dMRI/sub-001/dwi/noddi';
+cd(SubjPATH );
+
+addpath(genpath('/home/finn/Software/NODDI_toolbox_v1.05/'))
+addpath('/home/finn/Software/nifti_matlab/matlab/')
+
+%
+CreateROI('dwi_preproc_inorm.nii', 'mask.nii', 'NODDI_mask.mat');
+protocol = FSL2Protocol('dwi_preproc_inorm.bval', 'dwi_preproc_inorm.bvec');
+noddi = MakeModel('WatsonSHStickTortIsoV_B0');
+batch_fitting('NODDI_mask.mat', protocol, noddi, 'FittedParams_sub-001.mat', 4);
