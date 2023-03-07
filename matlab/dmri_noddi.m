7328cfcc5a39808d873249b7b78105850a594861
Finn Lennartsson <finn.lennartsson@gmail.com>
2023-03-07 10:07:31 +0900

    embryo to a noddi matlab routine

--- matlab/dmri_noddi.m
+++ matlab/dmri_noddi.m
@@ -1,4 +1,4 @@
-% cd to session dir and initialize 3D grey view 
+% cd data
 SubjPATH =  '/home/finn/Research/Projects/Kyoto_Collab/KPUM_NODDI/data/derivatives/dMRI/sub-001/dwi/noddi';
 cd(SubjPATH );
 
@@ -10,3 +10,4 @@ CreateROI('dwi_preproc_inorm.nii', 'mask.nii', 'NODDI_mask.mat');
 protocol = FSL2Protocol('dwi_preproc_inorm.bval', 'dwi_preproc_inorm.bvec');
 noddi = MakeModel('WatsonSHStickTortIsoV_B0');
 batch_fitting('NODDI_mask.mat', protocol, noddi, 'FittedParams_sub-001.mat', 4);
+SaveParamsAsNIfTI('FittedParams_sub-001.mat', 'NODDI_mask.mat', 'mask.nii', 'sub-001_desc-noddi')
