361236624e28ea3d4a6625cd88633e7069d7bb07
Finn Lennartsson <finn.lennartsson@gmail.com>
2023-03-07 15:56:14 +0900

    including session level

--- scripts/bids_sourcedata2bids.sh
+++ scripts/bids_sourcedata2bids.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-## KPUM NODDI
+# KPUM NODDI
 #
 usage()
 {
   base=$(basename "$0")
-  echo "usage: $base sID [options]
-Conversion of DCMs in /sourcedata into NIfTIs in /rawdata
-1. NIfTI-conversion to BIDS-compliant /rawdata folder
-2. validation of BIDS dataset
+  echo "usage: $base subjectID sessionID [options]
+Conversion of DICOMs to BIDS and validation of BIDS dataset
+The scripts uses Docker and heudiconv
+- DICOMs are expected to be in $studyfolder/sourcedata
+- Heuristics-files are located in code-subfolder $codedir/heudiconv_heuristics
+- NIfTIs are written into a BIDS-organised folder $studyfolder/rawdata
 
 Arguments:
-  sID				Subject ID (e.g. 001) 
+  sID				Subject ID (e.g. 002) 
+  ssID                       	Session ID (e.g. MR1)
 Options:
-  -f / -heuristic_file		Full path to heuristic file to use with heudiconv (default: \$codedir/heudiconv_heuristics/kpum_noddi.py)
+  -f / -heuristic_file		Full path to heuristic file to use with heudiconv (default: $codedir/heudiconv_heuristics/sub-sID_ses-ssID_kpum_noddi.py, or if does not exist $codedir/heudiconv_heuristics/kpum_noddi.py)
   -h / -help / --help           Print usage.
 "
   exit;
@@ -20,11 +23,28 @@ Options:
 
 ################ ARGUMENTS ################
 
-[ $# -ge 1 ] || { usage; }
+[ $# -ge 2 ] || { usage; }
 command=$@
 sID=$1
+ssID=$2
+shift; shift
 
-shift
+# Defaults
+codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+studydir=$PWD 
+rawdatadir=$studydir/rawdata
+dcmdir=$studydir/sourcedata
+
+if [ -f $codedir/heudiconv_heuristics/sub-${sID}_ses-${ssID}_kpum_noddi.py ]; then
+    heuristicfile=$codedir/heudiconv_heuristics/sub-${sID}_ses-${ssID}_kpum_noddi.py
+else
+    heuristicfile=$codedir/heudiconv_heuristics/kpum_noddi.py
+fi
+
+logdir=${studydir}/derivatives/preprocessing_logs/sub-${sID}/ses-${ssID}
+scriptname=`basename $0 .sh`
+
+# Read arguments
 while [ $# -gt 0 ]; do
     case "$1" in
 	-f|-heuristic_file)  shift; heuristicfile=$1; ;;
@@ -35,61 +55,51 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-# Define Folders
-codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-studydir=`pwd`
-rawdatadir=$studydir/rawdata;
-sourcedatadir=$studydir/sourcedata;
-heuristicfile=$codedir/heudiconv_heuristics/kpum_noddi.py
-scriptname=`basename $0 .sh`
-logdir=$studydir/derivatives/logs/sub-${sID}
-
 if [ ! -d $rawdatadir ]; then mkdir -p $rawdatadir; fi
 if [ ! -d $logdir ]; then mkdir -p $logdir; fi
 
 # We place a .bidsignore here
 if [ ! -f $rawdatadir/.bidsignore ]; then
-echo -e "# Exclude following from BIDS-validator\n" > $rawdatadir/.bidsignore;
+    echo -e "# Exclude following from BIDS-validator\n" > $rawdatadir/.bidsignore;
 fi
 
 # we'll be running the Docker containers as yourself, not as root:
 userID=$(id -u):$(id -g)
 
 ###   Get docker images:   ###
-docker pull nipy/heudiconv:latest  # should be changed to :latest when appropriate
+docker pull nipy/heudiconv:latest
 docker pull bids/validator:latest
+
+###   Extract DICOMs into BIDS:   ###
 
 # Get location and file for heuristic file
 heuristicdir=`dirname $heuristicfile`
 heuristicfile=`basename $heuristicfile`
 
-################ PROCESSING ################
-
-###   Extract DICOMs into BIDS:   ###
-# The images were extracted and organized in BIDS format:
-
+# Run heudiconv with docker container
 docker run --name heudiconv_container \
            --user $userID \
            --rm \
            -it \
            --volume $studydir:/base \
 	   --volume $codedir:/code \
-           --volume $sourcedatadir:/dataIn:ro \
 	   --volume $heuristicdir:/heuristic \
+           --volume $dcmdir:/dataIn:ro \
            --volume $rawdatadir:/dataOut \
            nipy/heudiconv \
-               -d /dataIn/sub-{subject}/*/*.dcm \
+               -d /dataIn/sub-{subject}/ses-{session}/*/*.dcm \
                -f /heuristic/$heuristicfile \
                -s ${sID} \
+               -ss ${ssID} \
                -c dcm2niix \
                -b \
                -o /dataOut \
                --overwrite \
-           > $logdir/sub-${sID}_$scriptname.log 2>&1 
+           > ${logdir}/sub-${sID}_ses-${ssID}_$scriptname.log 2>&1 
            
 # heudiconv makes files read only
 #    We need some files to be writable, eg for dHCP pipelines
-chmod -R u+wr,g+wr $rawdatadir
+sudo chmod -R u+wr,g+wr $rawdatadir
 
 
 # We run the BIDS-validator:
@@ -101,4 +111,5 @@ docker run --name BIDSvalidation_container \
                /data \
            > ${studydir}/derivatives/bids-validator_report.txt 2>&1
            #> ${logdir}/bids-validator_report.txt 2>&1                   
-           # For BIDS compliance, we want the validator report to go to the top level of derivatives. But for debugging, we want all logs from a given script to go to a script-specific fold
+           # For BIDS compliance, we want the validator report to go to the top level of derivatives. But for debugging, we want all logs from a given script to go to a script-specific folder
+         
---
+++ scripts/heudiconv_heuristics/template_minimalanonymization_sub-XXX_ses-YYY_kpum_noddi.py
@@ -0,0 +1,98 @@
+# Heuristics file
+# KPUM NODDI
+# Author: Finn Lennartsson 
+# Date: 2023-03-07
+#
+# In this template,
+# 1. adjust so it works with for the individual sub-sID_ses-ssID,
+# 2. save file as "sub-sID_ses-ssID_kpum_noddi.py"
+# 3. use it/used when calling bids_sourcedata2rawdata.sh
+#
+
+import os
+
+
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+    if template is None or not template:
+        raise ValueError('Template must be a valid format string')
+    return template, outtype, annotation_classes
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+
+    allowed template fields - follow python string module:
+
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+    # ANATOMY
+    t1wmprage = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-MPRAGE_run-{item:01d}_T1w')
+    t2starw = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-t2star_run-{item:01d}_T2starw')
+    swi = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-swi_run-{item:01d}_T2starw')
+    t2wtraclin = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-tra_run-{item:01d}_T2w')
+    flair = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-{item:01d}_FLAIR')
+    
+    # DWI
+    dki_ap = create_key('sub-{subject}/{session}/dwi/sub-{subject}_{session}_acq-dki_dir-AP_run-{item:01d}_dwi')
+    dti_ap = create_key('sub-{subject}/{session}/dwi/sub-{subject}_{session}_acq-dti_dir-AP_run-{item:01d}_dwi')
+
+    info = {t1wmprage: [], t2starw: [], swi:[], t2wtraclin: [], flair: [], dki_ap: [], dti_ap: []}
+    last_run = len(seqinfo)
+
+    for idx, s in enumerate(seqinfo):
+        """
+        The namedtuple `s` contains the following fields:
+
+        * total_files_till_now
+        * example_dcm_file
+        * series_id
+        * dcm_dir_name
+        * unspecified2
+        * unspecified3
+        * dim1
+        * dim2
+        * dim3
+        * dim4
+        * TR
+        * TE
+        * protocol_name
+        * is_motion_corrected
+        * is_derived
+        * patient_id
+        * study_description
+        * referring_physician_name
+        * series_description
+        * image_type
+        """
+
+
+        info = {t1wmprage: [], t2starw: [], swi:[], t2wtraclin: [], flair: [], dki_ap: [], dti_ap: []}
+
+        # ANATOMY
+        # 3D T1w Clinical scan (in collected in SAG plane)
+        if ('MPRAGE' in s.protocol_name) and ('ORIGINAL' in s.image_type): 
+            info[t1wmprage].append(s.series_id) # append if multiple series meet criteria   
+        # T2w Ax Clinical scan (in TRA plane)
+        if ('T2W_TRA' in s.protocol_name) and ('ORIGINAL' in s.image_type): 
+            info[t2wtraclin].append(s.series_id) # assign if a single series meets
+        # T2starw Clinical scan (in COR plane)
+        if ('T2starW_COR' in s.protocol_name) and ('ORIGINAL' in s.image_type): 
+            info[t2wcorclin].append(s.series_id) # append if multiple series meet criteria
+        # SWI Clinical scan (in TRA plane)
+        if ('SWI_TRA' in s.protocol_name) and ('ORIGINAL' in s.image_type): 
+            info[t2wcorclin].append(s.series_id) # append if multiple series meet criteria
+        # FLAIR
+        if ('FLAIR' in s.protocol_name) and ('ORIGINAL' in s.image_type): # takes normalized images
+            info[flair].append(s.series_id) # append if multiple series meet criteria
+        
+        # DIFFUSION
+        # DKI NODDI (dir-AP)
+        if ('DKI_2.5mm_NODDI_73dir' in s.series_description) and ('ORIGINAL' in s.image_type):
+            info[dki_ap].append(s.series_id) # append if multiple series meet criteria
+        # DTI Clinical scan (in TRA plane)
+        if ('DTI_2.5mm' in s.series_description) and ('ORIGINAL' in s.image_type):
+            info[dti_ap].append(s.series_id) # append if multiple series meet criteria
+                       
+    return info
