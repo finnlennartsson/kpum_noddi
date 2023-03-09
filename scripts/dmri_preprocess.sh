93bd7c4c59b74c07314101a911d3d1b52bd4a931
Finn Lennartsson <finn.lennartsson@gmail.com>
2023-03-09 19:47:10 +0900

    small updates

--- scripts/dmri_preprocess.sh
+++ scripts/dmri_preprocess.sh
@@ -17,6 +17,7 @@ Arguments:
 Options:
   -s / -session-file		Session file to depict which files should go into preprocessing. Overrides defaults below (default: derivatives/dMRI/sub-\$sID/ses-\$ssID/session_QC.tsv)
   -dwi				dMRI AP data (default: \$datadir/dwi/orig/sub-sID_ses-ssID_acq-dki_dir-AP_run-1_dwi.nii.gz)
+  -threads			Number of threads for MRtrix commands (default: 4)
   -d / -data-dir  <directory>   The directory used to output the preprocessed files (default: derivatives/dMRI/sub-sID/ses-ssID)
   -h / -help / --help           Print usage.
 "
@@ -36,6 +37,7 @@ currdir=$PWD
 # Defaults
 datadir=derivatives/dMRI/sub-$sID/ses-$ssID
 sessionfile=derivatives/dMRI/sub-$sID/ses-$ssID/session_QC.tsv
+threads=4
 
 if [ ! -f $sessionfile ]; then
     dwi=$datadir/dwi/orig/sub-${sID}_ses-${ssID}_acq-dki_dir-AP_run-1_dwi.nii.gz
@@ -49,6 +51,7 @@ while [ $# -gt 0 ]; do
     case "$1" in
 	-s|session-file) shift; sessionfile=$1; ;;
 	-dwi) shift; dwi=$1; ;;
+	-threads) shift; threads=$1; ;;
 	-d|-data-dir)  shift; datadir=$1; ;;
 	-h|-help|--help) usage; ;;
 	-*) echo "$0: Unrecognized option $1" >&2; usage; ;;
@@ -67,7 +70,8 @@ Subject:       	$sID
 Session:        $ssID
 Session file:	$sessionfile
 DWI (AP):	$dwi
-Directory:     	$datadir 
+Directory:     	$datadir
+ 
 $BASH_SOURCE   	$command
 ----------------------------"
 
@@ -77,7 +81,8 @@ if [ ! -d $logdir ]; then mkdir -p $logdir; fi
 
 echo dMRI preprocessing on subject $sID and session $ssID
 script=`basename $0 .sh`
-echo Executing: $codedir/$script.sh $command > ${logdir}/sub-${sID}_ses-${ssID}_$script.log 2>&1
+timestamp=`date`
+echo On $timestamp, executing: $codedir/$script.sh $command > ${logdir}/sub-${sID}_ses-${ssID}_$script.log 2>&1
 echo "" >> ${logdir}/sub-${sID}_ses-${ssID}_$script.log 2>&1
 echo "Printout $script.sh" >> ${logdir}/sub-${sID}_ses-${ssID}_$script.log 2>&1
 cat $codedir/$script.sh >> ${logdir}/sub-${sID}_ses-${ssID}_$script.log 2>&1
@@ -106,10 +111,10 @@ if [ -f $sessionfile ]; then
 	read
 	while IFS= read -r line
 	do
-	    # check if the file/image has passed QC (qc_pass_fail = fourth column)
+	    # check if the file/image has passed QC (qc_pass_fail = 4th column)
 	    QCPass=`echo "$line" | awk '{ print $4 }'`
 
-	    if [ $QCPass == 1 ]; then
+	    if [[ $QCPass == 1 || $QCPass == 0.5 ]]; then
 		
 		# Get file from column nbr 3
 		file=`echo "$line" | awk '{ print $3 }'`
@@ -117,7 +122,7 @@ if [ -f $sessionfile ]; then
 		filedir=`dirname $file`
 
 		#### Read flags in session.tsv file with corresponding column index
-		## DWI AP data
+		## DKI AP data (dMRI_DKI = 6th column)
 		dwiAP=`echo "$line" | awk '{ print $6 }'`
 		if [ $dwiAP == 1 ]; then		    
 		    if [ ! -f $datadir/dwi/preproc/dwiAP.mif.gz ]; then 
@@ -127,17 +132,17 @@ if [ -f $sessionfile ]; then
 		    fi
 		fi		
 		## b0AP and b0PA data
-		volb0AP=`echo "$line" | awk '{ print $7 }'`
+		volb0AP=`echo "$line" | awk '{ print $8 }'` #(dMRI_vol_for_b0AP = 8th column)
 		if [ ! $volb0AP == "-" ]; then
 		    b0APvol=$volb0AP #Remember this to later!!
-		    if [ ! -f $datadir/dwi/preproc/b0AP.mif.gz ]; then
+		    if [ ! -f $datadir/dwi/preproc/topup/b0AP.mif.gz ]; then
 			mrconvert $datadir/$filedir/$filebase.nii.gz -json_import $datadir/$filedir/$filebase.json - | \
 			    mrconvert -coord 3 $volb0AP -axes 0,1,2 - $datadir/dwi/preproc/topup/b0AP.mif.gz
 		    fi
 		fi
-		volb0PA=`echo "$line" | awk '{ print $8 }'`
+		volb0PA=`echo "$line" | awk '{ print $9 }'` #(dMRI_vol_for_b0PA = 9th column)
 		if [ ! $volb0PA == "-" ]; then
-		    if [ ! -f $datadir/dwi/preproc/b0PA.mif.gz ]; then
+		    if [ ! -f $datadir/dwi/preproc/topup/b0PA.mif.gz ]; then
 			mrconvert $datadir/$filedir/$filebase.nii.gz -json_import $datadir/$filedir/$filebase.json $datadir/dwi/preproc/topup/b0PA.mif.gz
 		    fi
 		fi
@@ -152,7 +157,7 @@ else
     mrconvert $filedir/$filebase.nii.gz \
 	      -json_import $filedir/$filebase.json \
 	      -fslgrad $filedir/$filebase.bvec $filedir/$filebase.bval  \
-	      $datadir/dwi/preproc/dwi.mif.gz
+	      $datadir/dwi/preproc/dwiAP.mif.gz
 fi
 
 
@@ -169,12 +174,16 @@ if [ ! -f topup/b0APPA.mif.gz ]; then
 	echo "Creating b0APPA.mif.gz from b0AP.mif.gz and b0PA.mif.gz"
 	mrcat topup/b0AP.mif.gz topup/b0PA.mif.gz topup/b0APPA.mif.gz
     else
-	echo "No b0APPA.mif.gz or pair of b0AP.mif.gz and b0PA.mif.gz are present to use with TOPUP 
-	1. Do this by put one good b0 from dir-AP_dwi and dir-PA_dwi into a file b0APPA.mif.gz into $datadir/dwi/preproc/topup
-	2. The same b0 from dir-AP_dwi should be put 1st in the dir-AP_dwi dataset, as dwifslpreprocess will use the 1st b0 in dir-AP and replace the first b0 in b0APPA with
-	3. Run this script again.    
-    	 "
-	exit;
+	if [ -f topup/b0AP.mif.gz ] && [ ! -f topup/b0PA.mif.gz ]; then
+	    echo "We only have b0AP.mif.gz in /topup => do not have a fieldmap, so eddy will be run without a TOPUP fieldmap"
+	else	
+	    echo "No b0APPA.mif.gz or pair of b0AP.mif.gz and b0PA.mif.gz are present to use with TOPUP
+	          1. Do this by putting one good b0 from dir-AP_dwi and dir-PA_dwi into a file b0APPA.mif.gz into $datadir/dwi/preproc/topup
+		  2. The same b0 from dir-AP_dwi should be put 1st in the dir-AP_dwi dataset, as dwifslpreprocess will use the 1st b0 in dir-AP and replace the first b0 in b0APPA with
+		  3. Run this script again.     
+    	 	  "
+	    exit;
+	fi
     fi
 fi
 
@@ -183,7 +192,7 @@ fi
 if [ ! -f dwi.mif.gz ]; then
     
     # 1. extract higher shells and put in a joint file
-    dwiextract -shells 1000,2600 dwiAP.mif.gz tmp_dwiAP_b1000b2600.mif
+    dwiextract -shells 1000,2000 dwiAP.mif.gz tmp_dwiAP_b1000b2600.mif
 	
     # 2. Sort out b0s
     # a) extract the b0 that will be used for TOPUP by
@@ -206,7 +215,7 @@ if [ ! -f dwi.mif.gz ]; then
     mrcat -axis 3 tmp_dwiAP_b0.mif tmp_dwiAP_b1000b2600.mif dwi.mif.gz
 
     # clean-up
-    rm tmp_dwi*.mif
+    rm tmp_dwi*.mif*
     
 fi
 
@@ -225,8 +234,7 @@ if [ ! -d denoise ]; then mkdir denoise; fi
 if [ ! -f dwi_den.mif.gz ]; then
     echo Doing MP PCA-denosing with dwidenoise
     # PCA-denoising
-    dwidenoise dwi.mif.gz dwi_den.mif.gz -noise denoise/dwi_noise.mif.gz -nthreads 4;
-    #dwidenoise dwi.mif.gz dwi_den.mif.gz -noise denoise/dwi_noise.mif.gz -nthreads 8;
+    dwidenoise dwi.mif.gz dwi_den.mif.gz -noise denoise/dwi_noise.mif.gz -nthreads $threads;
     # and calculate residuals
     mrcalc dwi.mif.gz dwi_den.mif.gz -subtract denoise/dwi_den_residuals.mif.gz
     echo Check the residuals! Should not contain anatomical structure
@@ -238,8 +246,7 @@ if [ ! -d unring ]; then mkdir unring; fi
 if [ ! -f dwi_den_unr.mif.gz ]; then
     echo Remove Gibbs Ringing Artifacts with mrdegibbs
     # Gibbs 
-    mrdegibbs -axes 0,1 dwi_den.mif.gz dwi_den_unr.mif.gz -nthreads 4
-    #mrdegibbs -axes 0,1 dwi_den.mif.gz dwi_den_unr.mif.gz -nthreads 8
+    mrdegibbs -axes 0,1 dwi_den.mif.gz dwi_den_unr.mif.gz -nthreads $threads
     #calculate residuals
     mrcalc dwi_den.mif.gz  dwi_den_unr.mif.gz -subtract unring/dwi_den_unr_residuals.mif.gz
     echo Check the residuals! Should not contain anatomical structure
@@ -256,30 +263,37 @@ cd $datadir/dwi/preproc
 scratchdir=dwifslpreproc
 
 if [ ! -f dwi_den_unr_eddy.mif.gz ];then
-
-if [ -f topup/b0APPA.mif.gz ]; then # We have b0APPA
-    dwifslpreproc -se_epi topup/b0APPA.mif.gz -rpe_header -align_seepi \
-		  -nocleanup \
-		  -scratch $scratchdir \
-		  -topup_options " --iout=field_mag_unwarped" \
-		  -eddy_options " --slm=linear --repol --mporder=8 --s2v_niter=10 --s2v_interp=trilinear --s2v_lambda=1 --estimate_move_by_susceptibility --mbs_niter=20 --mbs_ksp=10 --mbs_lambda=10 " \
-		  -eddyqc_all eddy \
-		  -nthreads 16 \
-		  -nthreads 8 \
-		  dwi_den_unr.mif.gz \
-		  dwi_den_unr_eddy.mif.gz;
-		  
-		  else
-		  
+    
+    if [ -f topup/b0APPA.mif.gz ]; then # We have b0APPA and can use TOPUP+EDDY
+	dwifslpreproc -se_epi topup/b0APPA.mif.gz -rpe_header -align_seepi \
+		      -nocleanup \
+		      -scratch $scratchdir \
+		      -topup_options " --iout=field_mag_unwarped" \
+		      -eddy_options " --slm=linear --repol --mporder=8 --s2v_niter=10 --s2v_interp=trilinear --s2v_lambda=1 --estimate_move_by_susceptibility --mbs_niter=20 --mbs_ksp=10 --mbs_lambda=10 " \
+		      -eddyqc_all eddy \
+		      -nthreads $threads \
+		      dwi_den_unr.mif.gz \
+		      dwi_den_unr_eddy.mif.gz;
+    else # We don't have b0APPA and cannot run TOPUP, and instead EDDY only with motion- and EC-correction
+	TRT=`mrinfo -property TotalReadoutTime dwi_den_unr.mif.gz`
+	PEdir=`mrinfo -property PhaseEncodingDirection dwi_den_unr.mif.gz`
+	dwifslpreproc -rpe_none -pe_dir $PEdir -readout_time $TRT \
+		      -nocleanup \
+		      -scratch $scratchdir \
+		      -eddy_options " --slm=linear --repol --mporder=8 --s2v_niter=10 --s2v_interp=trilinear --s2v_lambda=1 --mbs_niter=20 --mbs_ksp=10 --mbs_lambda=10 " \
+		      -eddyqc_all eddy \
+		      -nthreads $threads \
+		      dwi_den_unr.mif.gz \
+		      dwi_den_unr_eddy.mif.gz;
+    fi
+    # Now cleanup by transferring relevant files to topup folder and deleting scratch folder
+    mv eddy/quad ../../qc/.
+    cp $scratchdir/command.txt $scratchdir/log.txt $scratchdir/eddy_*.txt $scratchdir/applytopup_*.txt $scratchdir/slspec.txt eddy/.
+    mv $scratchdir/field_* $scratchdir/topup_* topup/.
+    rm -rf $scratchdir;
 fi
-# Now cleanup by transferring relevant files to topup folder and deleting scratch folder
-mv eddy/quad ../../qc/.
-cp $scratchdir/command.txt $scratchdir/log.txt $scratchdir/eddy_*.txt $scratchdir/applytopup_*.txt $scratchdir/slspec.txt eddy/.
-mv $scratchdir/field_* $scratchdir/topup_* topup/.
-rm -rf $scratchdir 
 
 cd $currdir
-
 
 ##################################################################################
 # 3. Mask generation, N4 biasfield correction, meanb0 generation and tensor estimation
@@ -302,10 +316,9 @@ fi
 
 # Do B1-correction. Use ANTs N4
 if [ ! -f  ${dwi}_N4.mif.gz ]; then
-    threads=10;
     if [ ! -d N4 ]; then mkdir N4;fi
     # Add number of threads used
-    dwibiascorrect ants -mask mask.mif.gz -bias N4/bias.mif.gz -nthreads 16 $dwi.mif.gz ${dwi}_N4.mif.gz
+    dwibiascorrect ants -mask mask.mif.gz -bias N4/bias.mif.gz -nthreads $threads $dwi.mif.gz ${dwi}_N4.mif.gz
 fi
 
 
@@ -330,10 +343,10 @@ if [ ! -f ${dwi}_inorm.mif.gz ]; then
     dwinormalise individual $dwi.mif.gz mask.mif.gz ${dwi}_inorm.mif.gz
 fi
 
-# Extract mean b0, b1000 and b2600
-for bvalue in 0 400 1000 2600; do
+# Extract mean b0, b1000 and b2000
+bvalues=`mrinfo -shell_bvalues ${dwi}_inorm.mif.gz`
+for bvalue in $bvalues; do
     bfile=meanb$bvalue
-
     if [ $bvalue == 0 ]; then
 	if [ ! -f $bfile.mif.gz ]; then
 	    dwiextract -shells $bvalue ${dwi}_inorm.mif.gz - |  mrmath -force -axis 3 - mean $bfile.mif.gz
